4ab9b47b2e93d234cf80cf92c43b564e7396ee8f
Felipe Rosa <felipemax.suporteti@gmail.com>
2023-10-23 21:49:07 +0100

    feat: product component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { ProductType } from '@/types/ProductType';
+import { Product } from './components/Product';
 
 async function getProducts() {
   const response = await fetch('https://fakestoreapi.com/products');
@@ -12,7 +13,6 @@ async function getProducts() {
 
 export default async function Home() {
   const products = await getProducts();
-  console.log({ products });
   /**
    * https://fakestoreapi.com/products
    * Every time that we use fetch data on the components, my component should be an async function.
@@ -22,10 +22,9 @@ export default async function Home() {
     <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
         {products?.map((product: ProductType) => (
-          <div key={product.id}>{product.title}</div>
+          <Product key={product.id} product={product} />
         ))}
       </div>
-      {/* <h1>Next E-Commerce 13</h1> */}
     </div>
   );
 }
